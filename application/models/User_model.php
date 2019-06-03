ac972e3ac64c5635052e725fcb4df053ff2d4153
Luis David Garcia Lunar <ldgl1215@gmail.com>
2019-06-02 20:27:58 -0400

    arreglo de error en modelos de usuario

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -38,7 +38,8 @@ class User_model extends CI_Model
 	public function check_username_exists($username)
 	{
 		$query = $this->db->get_where('users', array('username' => $username));
-		if (empty($query->row_array())) {
+		$tmp= $query->row_array();
+		if (empty($tmp)) {
 			return true;
 		} else {
 			return false;
@@ -50,7 +51,9 @@ class User_model extends CI_Model
 	public function check_email_exists($email)
 	{
 		$query = $this->db->get_where('users', array('email' => $email));
-		if (empty($query->row_array())) {
+
+		$tmp1= $query->row_array();
+		if (empty($tmp1)) {
 			return true;
 		} else {
 			return false;
